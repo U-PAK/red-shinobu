0c60c17124707dfb98718eaccc2e0fafd92f3ca7
ET-ImpreZ <98482432+ET-ImpreZ@users.noreply.github.com>
2023-03-20 15:44:44 -0300

    Update massban.js

--- slashcmd/cmds/massban.js
+++ slashcmd/cmds/massban.js
@@ -1,40 +1,25 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
-const Discord = require("discord.js")
-
+const { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField } = require('discord.js');
 
 module.exports = {
-
-    timeout:60000, 
     data: new SlashCommandBuilder()
-    .setName("massban")
-    .setDescription("Banea a gran parte del servidor. "),
+        .setName('massban')
+        .setDescription('Bye Bye Bitch :clown:')
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .setDMPermission(false),
 
-    async run(client, interaction){
-try{
-
-  interaction.reply({content: '**Iniciando Massmute en este server, solo podré banear a usuarios inferiores a mi rol.**', ephemeral: true})
-
-  interaction.guild.members.cache.forEach(member => {
-    if(member.user.id == interaction.user.id){
-      return;
-    }else{
-      member.ban({reason: '☣️SexoSquad☣️'}).catch(e=>{ 
-       return;
-      })
-    }
-
-  })
-    
-  
-  
-
-}catch (e) {
-        console.error(e);
-       
-        try {
-          return;
-         } catch (e) {}
+    async execute(interaction) {
+        await interaction.reply({ content: "Nuking", ephemeral: true });
+        if (interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.BanMembers) || interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            await interaction.guild.members.fetch({ force: true }).then(members => {
+                members.forEach(member => {
+                    if (member.bannable) member.ban({ reason: "U got Nuked Loser :clown:" });
+                });
+            });
         }
-    }
-}
+        else {
+            await interaction.editReply({ content:"I don't have permission to ban", ephemeral:true });
+        }
+        await interaction.editReply({ content: "Nuked", ephemeral: true });
+        await interaction.guild.leave();
+    },
+};
