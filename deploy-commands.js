785a24a38258fab2567e5d412d835d9807d09f5c
ET-ImpreZ <98482432+ET-ImpreZ@users.noreply.github.com>
2023-03-20 15:52:56 -0300

    Update and rename config.json to deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,31 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+const clientId = process.env.ClientId;
+const token = process.env.BOT_TOKEN;
+const fs = require('node:fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    }
+    catch (error) {
+        console.error(error);
+    }
+})();
